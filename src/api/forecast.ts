40991d8124d7667998c2c93cd34429701a9bf875
Aleksandr Prikhodko <kakto228030904@gmail.com>
2026-09-12 14:33:32 +0300

    feat(api-client): add url constructor, abort controller and error handling

--- src/api/forecast.ts
+++ src/api/forecast.ts
@@ -1,13 +1,20 @@
 import 'dotenv/config';
 import type { forecastRespondType } from '../types.js';
+import { fetchWrapper } from './utils.ts';
 
 export const getForecast = async (
 	lat: number,
 	long: number,
 	days = 3,
 ): Promise<forecastRespondType> => {
-	const responce = await fetch(
-		`${process.env.BASE_FORECAST_URL}/forecast?latitude=${lat}&longitude=${long}&daily=temperature_2m_max,temperature_2m_min,precipitation_sum&forecast_days=${days}&timezone=auto`,
+	const url = new URL('forecast', process.env.BASE_FORECAST_URL);
+	url.searchParams.set('latitude', lat.toString());
+	url.searchParams.set('longitude', long.toString());
+	url.searchParams.set(
+		'daily',
+		'temperature_2m_max,temperature_2m_min,precipitation_sum',
 	);
-	return responce.json() as Promise<forecastRespondType>;
+	url.searchParams.set('forecast_days', days.toString());
+	url.searchParams.set('timezone', 'auto');
+	return await fetchWrapper<forecastRespondType>(url);
 };
---
+++ src/api/utils.ts
@@ -0,0 +1,28 @@
+import 'dotenv/config';
+import { HttpError, JSONError, NetworkError, TimeoutError } from '../error.ts';
+
+export const fetchWrapper = async <T>(url: URL): Promise<T> => {
+	const controller = new AbortController();
+	const timeoutId = setTimeout(
+		() => controller.abort(),
+		Number(process.env.TIMEOUT),
+	);
+	let responce: Response;
+	try {
+		responce = await fetch(url);
+		clearTimeout(timeoutId);
+	} catch (error) {
+		if (!(error instanceof Error)) throw error;
+		if (error.name === 'AbortError')
+			throw new TimeoutError('Превышено время ожидания');
+		else if (error.name === 'SyntaxError') throw new JSONError('Битый JSON');
+		throw new NetworkError('Проблема с сетью');
+	}
+	if (!responce.ok) {
+		if (responce.status >= 500) {
+			throw new HttpError(responce.status, 'Проблема с сервером');
+		}
+		throw new HttpError(responce.status, 'Проблема со стороны клиента');
+	}
+	return responce.json() as T;
+};
